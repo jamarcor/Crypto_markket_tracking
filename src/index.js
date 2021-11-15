ca87193b0718a3809771d4c03ac5b305f61bfff8
Anthony <tony.hongq@gmail.com>
2021-11-15 15:13:43 -0800

    Add README.md

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hi");
+const api =  "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=CNY&apikey=demo";
+// let userData;
+
+fetch(api)
+.then(response => response.json())
+.then(json => renderData);
+
+function renderData(info){
+    console.log(info)
+}
+
+renderData()
